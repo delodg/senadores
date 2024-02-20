1ceb44ee4a852677030fe9ac960a01b2b80ada02
delodg <delineiro@gmail.com>
2024-02-20 01:09:58 -0300

    eliminé la palabra telefono

--- src/SenadorCard.jsx
+++ src/SenadorCard.jsx
@@ -15,8 +15,8 @@ const SenadorCard = ({ senador }) => {
         <li className="mb-3"><i className="ri-map-pin-line mr-2"></i>Provincia: {senador.PROVINCIA}</li>
         <li className="mb-3"><i className="ri-calendar-check-line mr-2"></i>C. Legal: {senador.C_LEGAL}</li>
         <li className="mb-3"><i className="ri-time-line mr-2"></i>D. Legal: {senador.D_LEGAL}</li>
-        {senador.EMAIL && <li className="mb-3"><i className="ri-mail-line mr-2"></i>Email: {senador.EMAIL}</li>}
-        {senador.TELEFONO && <li className="mb-3"><i className="ri-phone-line mr-2"></i>Teléfono: {senador.TELEFONO}</li>}
+        {senador.EMAIL && <li className="mb-3"><i className="ri-mail-line mr-2"></i>{senador.EMAIL}</li>}
+        {senador.TELEFONO && <li className="mb-3"><i className="ri-phone-line mr-2"></i>{senador.TELEFONO}</li>}
         {senador.FACEBOOK && <li className="mb-3"><i className="ri-facebook-box-fill mr-2"></i><a href={senador.FACEBOOK} className="hover:text-blue-600">Facebook</a></li>}
         {senador.INSTAGRAM && <li className="mb-3"><i className="ri-instagram-line mr-2"></i><a href={senador.INSTAGRAM} className="hover:text-pink-600">Instagram</a></li>}
         {senador.TWITTER && <li className="mb-3"><i className="ri-twitter-line mr-2"></i><a href={senador.TWITTER} className="hover:text-blue-400">Twitter</a></li>}
